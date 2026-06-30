72ceaddd2b68d3336f90222c62f3e7d081ed7257
ZZTX <3809026566@qq.com>
2026-06-30 08:44:36 +0800

    Update

--- worker.js
+++ worker.js
@@ -1,5 +1,23 @@
 export default {
   async fetch(request) {
-    return new Response("This is a image repository");
+    // 商品数据
+    const productList = [
+      {
+        "title": "手机",
+        "subtitle": "云端精选商品",
+        "price": "6999",
+        "tag": "推荐",
+        "imageUrl": "https://picsum.photos/id/3/300/300"
+      }
+    ];
+
+    return new Response(JSON.stringify(productList), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS",
+        "Access-Control-Allow-Headers": "*"
+      }
+    });
   }
 }
